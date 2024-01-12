0ce4bfdbcde2d267f789d5a50bf398e567592884
Oleksandra Antonova <antonova.ssh@gmail.com>
2024-01-12 21:02:37 +0100

    added css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () =>
 {
-    return <header className = 'header'>
+    return <header className = {s.header}>
     <img src='https://img.freepik.com/premium-vector/cute-hand-drawn-cat-paws-white-background-vector-adorable-animals-silhouette-trendy-style-funny-cute-hygge-illustration-poster-banner-print-decoration-kids-playroom_514409-1359.jpg'/> 
    </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () =>
 {
-    return <div className='content'>
+    return <div className={s.content}>
     <img src='https://img.freepik.com/premium-photo/group-dogs-running-field_902639-15522.jpg'></img>
   <div>
     ava + description
@@ -12,11 +13,11 @@ const Profile = () =>
     <div>
       New post
     </div>
-    <div>
-      <div>
+    <div className = {s.posts}>
+      <div className={s.item}>
         Post 1
       </div>
-      <div>
+      <div className={s.item}>
         Post 2
       </div>
     </div>
