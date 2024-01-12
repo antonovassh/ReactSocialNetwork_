2e46a56cfa35f9d522c55178a1c3989d7e3e101b
Oleksandra Antonova <antonova.ssh@gmail.com>
2024-01-12 18:04:27 +0100

    started main page design

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () =>
+{
+    return <header className = 'header'>
+    <img src='https://img.freepik.com/premium-vector/cute-hand-drawn-cat-paws-white-background-vector-adorable-animals-silhouette-trendy-style-funny-cute-hygge-illustration-poster-banner-print-decoration-kids-playroom_514409-1359.jpg'/> 
+   </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () =>
+{
+    return <div className='content'>
+    <img src='https://img.freepik.com/premium-photo/group-dogs-running-field_902639-15522.jpg'></img>
+  <div>
+    ava + description
+  </div>
+  <div>
+    My posts
+    <div>
+      New post
+    </div>
+    <div>
+      <div>
+        Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+  </div>
+  </div>
+}
+
+export default Profile;
